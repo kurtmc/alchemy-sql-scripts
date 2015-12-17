496c177400c7beca9abaa10682cc911a3272d56f
Kurt McAlpine <kurt@linux.com>
2015-12-18 09:40:31 +1300

    Query now makes more sense

--- Sales-stats.sql
+++ Sales-stats.sql
@@ -1,6 +1,15 @@
-SELECT a."Salesperson Code", c."Name", SUM(b."Outstanding Amount")
-FROM NAVTEST2009R2.dbo."TEST Alchemy Agencies Ltd$Sales Header" AS a
-JOIN NAVTEST2009R2.dbo."TEST Alchemy Agencies Ltd$Sales Line" AS b ON a."No_" = b."Document No_"
-JOIN NAVTEST2009R2.dbo."TEST Alchemy Agencies Ltd$Salesperson_Purchaser" AS c ON a."Salesperson Code" = c.Code
-WHERE a."Order Date" >= '2014-01-01 00:00:00' and a."Shipment Date" <= '2015-01-01 00:00:00'
-GROUP BY a."Salesperson Code", c."Name"
+SELECT a."Salesperson Code", b."Name", a."Amount"
+
+FROM 
+
+(
+	SELECT a."Salesperson Code", SUM(b."Outstanding Amount") AS "Amount"
+	FROM NAVTEST2009R2.dbo."TEST Alchemy Agencies Ltd$Sales Header" AS a
+	JOIN NAVTEST2009R2.dbo."TEST Alchemy Agencies Ltd$Sales Line" AS b
+		ON a."No_" = b."Document No_"
+	WHERE a."Order Date" >= '2014-01-01 00:00:00' and a."Shipment Date" <= '2015-01-01 00:00:00'
+	GROUP BY a."Salesperson Code"
+) AS a
+
+JOIN NAVTEST2009R2.dbo."TEST Alchemy Agencies Ltd$Salesperson_Purchaser" AS b ON a."Salesperson Code" = b.Code
+
