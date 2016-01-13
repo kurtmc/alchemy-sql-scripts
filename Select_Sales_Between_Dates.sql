353b3bd4edd9415755bf4b0525e850f18442ad60
Kurt McAlpine <kurt@linux.com>
2016-01-14 10:56:55 +1300

    Cast to date

--- Select_Sales_Between_Dates.sql
+++ Select_Sales_Between_Dates.sql
@@ -1,7 +1,7 @@
 DECLARE @StartDate Date
 DECLARE @EndDate Date
-SET @StartDate = '2014-04-01 00:00:00'
-SET @EndDate   = '2015-03-31 00:00:00'
+SET @StartDate = '2014-04-01'
+SET @EndDate   = '2015-03-31'
 
 
 SELECT b.Code, b."Name", a."Amount"
@@ -15,8 +15,10 @@ FROM (
 		JOIN NAVLIVE.dbo."Alchemy Agencies Ltd$Sales Invoice Line" AS b
 		ON a."No_" = b."Document No_"
 	) AS a
-	WHERE a."Order Date" >= @StartDate and a."Order Date" <= @EndDate
+	WHERE (CAST(a."Order Date" AS date) >= CAST(@StartDate AS date)) AND (CAST(a."Order Date" AS date) <= CAST(@EndDate AS date))
 	GROUP BY "Salesperson Code"
 ) AS a
 JOIN NAVLIVE.dbo."Alchemy Agencies Ltd$Salesperson_Purchaser" AS b
 ON a."Salesperson Code" = b.Code
+ORDER BY b.Code
+
